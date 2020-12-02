9cfaa3967642e02f0d23bc2f80b189883b984935
Batch-21 ForLearning <samadigithub@gmail.com>
2020-12-02 08:54:44 -0500

    Initial commit

---
+++ src/practiceToLearn/FromIjToGit.java
@@ -0,0 +1,9 @@
+package practiceToLearn;
+
+public class FromIjToGit {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Today is the the 3rd Day for Git Class");
+
+    }
+}
