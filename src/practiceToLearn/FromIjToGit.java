5da8aacceb348fde3ef53965a137148c84f82f2d
Batch-21 ForLearning <samadigithub@gmail.com>
2020-12-02 09:17:53 -0500

    Working on nice message

--- src/practiceToLearn/FromIjToGit.java
+++ src/practiceToLearn/FromIjToGit.java
@@ -4,6 +4,6 @@ public class FromIjToGit {
     public static void main(String[] args) {
         // write your code here
         System.out.println("Today is the the 3rd Day for Git Class");
-
+        //B21 is Great in Java
     }
 }
